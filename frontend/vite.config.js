58fce83b20b66edab9e924b4395051eb9eabdc26
franckhbz10 <u201421134@upc.edu.pe>
2025-11-01 18:10:03 -0500

    fix: bcrypt warning, lexicon 404

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://miappbora-production.up.railway.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
